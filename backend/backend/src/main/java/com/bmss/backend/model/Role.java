7a077386d8ea54c143361acf5fd66a02728a546f
Samuel Rodrigues de Oliveira Neves <68709646+SamuelRNeves@users.noreply.github.com>
2025-10-15 09:17:07 -0300

    Remove unused controller AuthRequest stub

--- backend/backend/src/main/java/com/bmss/backend/model/Role.java
+++ backend/backend/src/main/java/com/bmss/backend/model/Role.java
@@ -10,8 +10,7 @@ public class Role {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public String getName() {
+        return name;
+    }
 }
