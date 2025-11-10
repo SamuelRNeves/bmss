49a5eb51a4f1d242577e1b410d447b13e72f5b32
Samuel <samuelneves538@gmail.com>
2025-11-10 14:30:05 -0300

    mapeando 10

--- backend/backend/src/main/java/com/bmss/backend/controller/AuthController.java
+++ backend/backend/src/main/java/com/bmss/backend/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping({"/api/v1/auth", "/auth"})
+@RequestMapping("/api/v1/auth")
 @CrossOrigin(
         origins = {
                 "http://localhost:3000",
