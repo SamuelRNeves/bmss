7c4d5cd6b465e27ece2dfe2854896a3690bd825d
Samuel Rodrigues de Oliveira Neves <68709646+SamuelRNeves@users.noreply.github.com>
2025-11-12 08:31:00 -0300

    Enable quick profile switching in header

--- backend/backend/src/main/java/com/bmss/backend/repository/ItemRepository.java
+++ backend/backend/src/main/java/com/bmss/backend/repository/ItemRepository.java
@@ -13,11 +13,13 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
     List<Item> findBySourceNameNotContainingIgnoreCase(String source, Pageable pageable);
     List<Item> findBySourceNameContainingIgnoreCase(String sourceName, Pageable pageable);
     List<Item> findTop20BySourceNameOrderByPublishedAtDesc(String sourceName);
+    List<Item> findTop50ByIsTweetFalseOrIsTweetIsNullOrderByPublishedAtDesc();
     boolean existsByUrl(String url);
 
     // 🔹 NOVOS MÉTODOS PARA TWEETS
     List<Item> findByIsTweetTrueOrderByPublishedAtDesc(Pageable pageable);
     List<Item> findTop20ByIsTweetTrueOrderByPublishedAtDesc();
+    List<Item> findTop50ByIsTweetTrueOrderByPublishedAtDesc();
     List<Item> findTop20ByIsTweetFalseOrderByPublishedAtDesc();
     boolean existsByTweetId(String tweetId);
 }
